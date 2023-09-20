4319ca368f7346bf3f24cb929a05613259f9578b
mbeal_sgp6gtp <mbeal_sgp6gtp@100.66.154.76>
2023-09-20 14:30:05 -0400

    create myArrayListIterator.java

---
+++ myArrayListIterator.java
@@ -0,0 +1,21 @@
+
+public interface myArrayListIterator {
+	
+	public void add(Fraction frac);
+	
+	public boolean hasNext();
+	
+	public boolean hasPrevious();
+	
+	public Fraction next();
+	
+	public int nextIndex();
+	
+	public Fraction previous();
+	
+	public int previousIndex();
+	
+	public void remove();
+	
+	public void set(Fraction frac)
+}
