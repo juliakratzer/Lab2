ada9c26bcef70f8a6d43620cdf18b52ba7f9166b
jovee <jove.emmons@maine.edu>
2023-09-27 00:57:04 -0400

    previousIndex and remove methods added

--- myArrayListIterator.java
+++ myArrayListIterator.java
@@ -1,21 +1,88 @@
+import java.util.*;
 
-public interface myArrayListIterator {
+public class myArrayListIterator implements Iterator<Fraction>{
+	private int previousIndex;
+	private int nextIndex = 0;
+	private myArrayList arr;
+	private boolean nextWasLast;
 	
-	public void add(Fraction frac);
 	
-	public boolean hasNext();
+	public myArrayListIterator(myArrayList arrList) {
+		arr = arrList;
+	}
 	
-	public boolean hasPrevious();
 	
-	public Fraction next();
+	public void add(Fraction frac) {
+		Fraction stored = frac;
+
+		if(arr.myArray[arr.myArray.length-1] != null) {
+			arr.grow();
+		}
+		for(int i = nextIndex; i<arr.myArray.length;i++) {
+//			System.out.println(i);
+//			System.out.println(arr.myArray.length);
+			Fraction keep = arr.myArray[i];
+			arr.myArray[i] = stored;
+			stored = keep;
+			
+		}
+		
+	}
 	
-	public int nextIndex();
+	public boolean hasNext() {
+		if(nextIndex<arr.length()) {
+			return true;
+		}
+		return false;
+	}
 	
-	public Fraction previous();
+	public boolean hasPrevious() {
+		if(previousIndex>=0) {
+			return true;
+		}
+		return false;
+	}
 	
-	public int previousIndex();
+	public Fraction next() {
+		nextWasLast = true;
+		return null;
+	}
 	
-	public void remove();
+	public int nextIndex() {
+		return 0;
+	}
 	
-	public void set(Fraction frac);
+	public Fraction previous() {
+		nextWasLast = false;
+		return null;
+	}
+	
+	/**
+	 * returns the index of the item that will be returned by the next call to previous
+	 * @author JoveEUSM
+	 * 
+	 * 
+	 * @return the index of the item returned by the next call to previous(). if there would be no item it returns -1
+	 */
+	public int previousIndex() {
+		if(previousIndex>-1) {
+			return previousIndex; 
+		}
+		return -1;
+	}
+	
+	/**
+	 * removes the last item returned by a call to either next previous, whichever was more recent
+	 * @author JoveEUSM 
+	 * 
+	 */
+	public void remove() {
+		if(nextWasLast) {
+			arr.remove(arr.myArray[nextIndex]);
+		}
+		arr.remove(arr.myArray[lastIndex]);
+	}
+	
+	public void set(Fraction frac) {
+	}
 }
